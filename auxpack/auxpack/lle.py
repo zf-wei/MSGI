690dd541cc43afe67a5d2fb481cc5c1ab405e1b9
zf-wei <zfwei11@gmail.com>
2023-07-12 23:42:43 -0400

    V7.12.3

--- auxpack/auxpack/lle.py
+++ auxpack/auxpack/lle.py
@@ -18,3 +18,16 @@ def lle(graph, dim):
     v = v[:, idx]
     embedding = v[:, 1:(dim+1)]
     return embedding
+
+import scipy.sparse as sp
+import scipy.sparse.linalg as splg
+def lles(graph, dim):
+    A = nx.to_scipy_sparse_matrix(graph, weight='weight')
+    A = normalize(A, norm='l1', axis=1)
+    I_n = sp.eye(graph.number_of_nodes())
+    I_min_A = (I_n - A).T.dot(I_n - A)
+    w, v = splg.eigs(I_min_A, k=dim + 1, which='SM')
+    idx = np.argsort(w.real)
+    v = v[:, idx]
+    embedding = v[:, 1:(dim + 1)]
+    return embedding.real
